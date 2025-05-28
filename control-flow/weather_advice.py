69357769bfadc26357189748c12c4552317c464b
Alpatson Cobbina Siaw <cobbinaalpatson@gmail.com>
2025-05-28 09:08:58 +0000

    Add weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,9 +4,12 @@ response = input("What's the weather like today? (sunny/rainy/cold): ").strip().
 
 if response == "sunny":
     print("Wear a t-shirt and sunglasses.")
+    
 elif response == "rainy":
     print("Don't forget your umbrella and a raincoat.")
+    
 elif response == "cold":
     print("Make sure to wear a warm coat and a scarf.")
+    
 else:
     print("Sorry, I don't have recommendations for this weather.")
