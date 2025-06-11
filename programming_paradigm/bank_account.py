1bb88a6d793d9a411ee8b9103657d1b27e81867d
Alpatson Cobbina Siaw <cobbinaalpatson@gmail.com>
2025-06-11 22:44:58 +0000

    Add bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -31,5 +31,5 @@ class BankAccount:
             self.account_balance -= amount
             return True
 
-    def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+      def display_balance(self):
+        print(f"Current Balance: ${self.account_balance:.2f}")
